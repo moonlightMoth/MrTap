fbc675a1963dab27ea2ef872fc6d5308a16672ee
VETKA_CHERESHNY <ghjpa@inbox.ru>
2017-06-07 10:46:00 +0300

    with pixels

--- app/src/main/java/com/example/user/clicker1/MainActivity.java
+++ app/src/main/java/com/example/user/clicker1/MainActivity.java
@@ -11,41 +11,63 @@ import android.widget.Button;
 
 import java.util.Random;
 
-
 public class MainActivity extends AppCompatActivity {
     public int min = 150;
     public int wid;
     public int hei;
+    private int maxNOTTD; // max and min number of taps on circle to
+    private int minNOTTD; // destroy it
 
-    private int clicked = 6;
+    private int clicked;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)  {
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         DisplayMetrics displayMetrics = new DisplayMetrics();
+
         Button mbutton = (Button) findViewById(R.id.round);
-        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE); // the results will be higher than using the activity context object or the getWindowManager() shortcut
+
+        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getMetrics(displayMetrics);
-        mbutton.setX((displayMetrics.widthPixels)/4);
-        mbutton.setY((displayMetrics.heightPixels)/4);
-        wid = displayMetrics.widthPixels;
-        hei = displayMetrics.heightPixels;
+
+        wid = (int)displayMetrics.widthPixels;
+        hei =  (int)displayMetrics.heightPixels;
+
+        Log.d("asd", Integer.toString(wid));
+
+
+
+        maxNOTTD = 1;
+        minNOTTD = 1;
+
+        clicked = new Random().nextInt(maxNOTTD-minNOTTD+1)+minNOTTD;
+        mbutton.setX(new Random().nextInt(wid-100));
+        mbutton.setY(new Random().nextInt(hei-100));
         mbutton.setText(Integer.toString(clicked));
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clicked--;
                 Button butt = (Button) findViewById(R.id.round);
                 butt.setText(Integer.toString(clicked));
+
                 if (clicked==0) {
                     butt.setVisibility(View.GONE);
                     Log.d("button", "clicked");
 
-                    butt.setX(new Random().nextInt(wid-400));
-                    butt.setY(new Random().nextInt(hei-650));
-                    clicked = new Random().nextInt(3)+1;
+                    butt.setX(new Random().nextInt(wid-400)); //redo
+                    butt.setY(new Random().nextInt(hei-650)); //
+
+//vars.setMinNumOfTapsToDestroy(vars.getMinNumOfTapsToDestroy()+2);
+//vars.setMaxNumOfTapsToDestroy(vars.getMaxNumOfTapsToDestroy()+2);
+//minNOTTD+=2;
+// maxNOTTD+=2;
+
+                    clicked = new Random().nextInt(maxNOTTD-minNOTTD+1)+minNOTTD;
                     butt.setText(Integer.toString(clicked));
                     butt.setVisibility(View.VISIBLE);
                 }
@@ -54,5 +76,4 @@ public class MainActivity extends AppCompatActivity {
         mbutton.setOnClickListener(listener);
     }
 
-
-    }
+}
