7ab53e3036266cbf60ce459a450dbb0eefb7a542
SalavursA <artempetr22>
2017-06-08 14:28:14 +0300

    fuck this shit im out xD
    gotovoe dlya pokaza

--- app/src/main/java/com/example/user/clicker1/Shop.java
+++ app/src/main/java/com/example/user/clicker1/Shop.java
@@ -2,6 +2,7 @@ package com.example.user.clicker1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Shop extends AppCompatActivity {
 
@@ -9,5 +10,30 @@ public class Shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+
+        findViewById(R.id.appClickPerTapNormal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vars vars = new Vars();
+                vars.setNumOfClicksPerTap(1);
+            }
+        });
+
+        findViewById(R.id.appClickPerTap1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vars vars = new Vars();
+                vars.setNumOfClicksPerTap(2);
+            }
+        });
+
+        findViewById(R.id.appClickPerTap2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vars vars = new Vars();
+                vars.setNumOfClicksPerTap(3);
+            }
+        });
     }
 }
