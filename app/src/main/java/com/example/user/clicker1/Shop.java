f8ca6881c92545616572269d669129cacbc3a404
SalavursA <artempetr22>
2017-06-08 14:41:23 +0300

    fuck this shit im out xD
    gotovoe dlya pokaza

--- app/src/main/java/com/example/user/clicker1/Shop.java
+++ app/src/main/java/com/example/user/clicker1/Shop.java
@@ -1,8 +1,10 @@
 package com.example.user.clicker1;
 
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Shop extends AppCompatActivity {
 
@@ -11,12 +13,21 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        final Button one=(Button)findViewById(R.id.appClickPerTapNormal);
+        final Button two=(Button)findViewById(R.id.appClickPerTap1);
+        final Button three=(Button)findViewById(R.id.appClickPerTap2);
+
 
         findViewById(R.id.appClickPerTapNormal).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Vars vars = new Vars();
                 vars.setNumOfClicksPerTap(1);
+
+                one.setBackground(getDrawable(R.drawable.activated_button));
+                two.setBackground(getDrawable(R.drawable.button_shop));
+                three.setBackground(getDrawable(R.drawable.button_shop));
+
             }
         });
 
@@ -25,6 +36,10 @@ public class Shop extends AppCompatActivity {
             public void onClick(View v) {
                 Vars vars = new Vars();
                 vars.setNumOfClicksPerTap(2);
+
+                one.setBackground(getDrawable(R.drawable.button_shop));
+                two.setBackground(getDrawable(R.drawable.activated_button));
+                three.setBackground(getDrawable(R.drawable.button_shop));
             }
         });
 
@@ -33,6 +48,10 @@ public class Shop extends AppCompatActivity {
             public void onClick(View v) {
                 Vars vars = new Vars();
                 vars.setNumOfClicksPerTap(3);
+
+                one.setBackground(getDrawable(R.drawable.button_shop));
+                two.setBackground(getDrawable(R.drawable.button_shop));
+                three.setBackground(getDrawable(R.drawable.activated_button));
             }
         });
     }
