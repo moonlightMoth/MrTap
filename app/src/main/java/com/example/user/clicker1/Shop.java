04399fd8edfc697c4d83b3ccb9b78aa8bf091bbb
VETKA_CHERESHNY <ghjpa@inbox.ru>
2017-06-09 13:49:37 +0300

    add project's config

--- app/src/main/java/com/example/user/clicker1/Shop.java
+++ app/src/main/java/com/example/user/clicker1/Shop.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Shop extends AppCompatActivity {
 
@@ -15,9 +16,13 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+
+        final Vars vars = new Vars();
         final Button one=(Button)findViewById(R.id.appClickPerTapNormal);
         final Button two=(Button)findViewById(R.id.appClickPerTap1);
         final Button three=(Button)findViewById(R.id.appClickPerTap2);
+        final TextView amount = (TextView) findViewById(R.id.textView);
+        amount.setText(vars.getCoins());
 
 
         findViewById(R.id.appClickPerTapNormal).setOnClickListener(new View.OnClickListener() {
