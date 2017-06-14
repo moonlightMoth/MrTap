5a7527e77dc9d0ebf6e25cde16ce4d1e9dc5368d
VETKA_CHERESHNY <ghjpa@inbox.ru>
2017-06-14 14:38:24 +0300

    zaebissstsssssslit

--- app/src/main/java/com/example/user/clicker1/Game.java
+++ app/src/main/java/com/example/user/clicker1/Game.java
@@ -1,16 +1,20 @@
 package com.example.user.clicker1;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -18,6 +22,21 @@ import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
+    final CountDownTimer the=new CountDownTimer(Integer.MAX_VALUE, 5) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            ProgressBar a=(ProgressBar)findViewById(R.id.progressBar);
+            a.setProgress(a.getProgress()-1);
+            if (a.getProgress()<=0) endOfGame();
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    };
+    final Game this_=this;
+    final int DIALOG_EXIT=1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +60,8 @@ public class Game extends AppCompatActivity {
         WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getMetrics(displayMetrics);
 
-
+        vars.setPops(0);
+        vars.setTaps(0);
         vars.setDispalyHeight(displayMetrics.heightPixels);
         vars.setDispalyWidth(displayMetrics.widthPixels);
 
@@ -58,8 +78,16 @@ public class Game extends AppCompatActivity {
         bar.setProgress(1000);
 
 
-        new Timer(Integer.MAX_VALUE, 5, bar).start();
-
+        the.start();
+        Button Pause=(Button)findViewById(R.id.Pause);
+        Pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                the.cancel();
+                Dialog dialog=onCreateDialog(DIALOG_EXIT);
+                dialog.show();
+            }
+        });
 
 
         setButtVision(greenButton,yellowButton,popsField,tapsField, bar,bomb,coins,heal);
@@ -102,7 +130,6 @@ public class Game extends AppCompatActivity {
         findViewById(R.id.toMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(this_,MainActivity.class);
                 finish();
 
             }
@@ -155,6 +182,46 @@ public class Game extends AppCompatActivity {
 
         }
     }
+
+    protected Dialog onCreateDialog (int id)
+    {
+        if (id == DIALOG_EXIT) {
+            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+            adb.setTitle("Game Paused");
+
+            adb.setPositiveButton("Resume", myClickListener);
+            adb.setNegativeButton("Restart", myClickListener);
+            adb.setNeutralButton("Main Menu", myClickListener);
+            adb.setCancelable(false);
+            AlertDialog dialog = adb.create();
+
+            return dialog;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    protected DialogInterface.OnClickListener myClickListener = new DialogInterface.OnClickListener()
+    {
+        public void onClick(DialogInterface dialog, int which)
+        {
+            switch (which)
+            {
+                case Dialog.BUTTON_POSITIVE:
+                    the.start();
+                    break;
+                case Dialog.BUTTON_NEGATIVE:
+                    Intent i=new Intent(this_, Game.class);
+                    finish();
+                    startActivity(i);
+                    break;
+                case Dialog.BUTTON_NEUTRAL:
+                    Intent intent = new Intent(this_, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 }
 
 
